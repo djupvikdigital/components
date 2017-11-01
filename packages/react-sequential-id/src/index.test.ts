178e55ec5408dfb38ae6d64862b59356a0e45c67
Reidar Djupvik <mail@3rdhand.info>
2017-11-01 19:59:25 +0000

    Stop using synthetic default imports

--- packages/react-sequential-id/src/index.test.ts
+++ packages/react-sequential-id/src/index.test.ts
@@ -1,18 +1,18 @@
-import Enzyme from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import { configure, shallow } from 'enzyme'
+import Adapter = require('enzyme-adapter-react-16')
 import { createElement as r } from 'react'
-import uniqueid from 'uniqueid'
+import uniqueid = require('uniqueid')
 
 import { withIdFactory } from './'
 
-Enzyme.configure({ adapter: new Adapter() })
+configure({ adapter: new Adapter() })
 
 describe('withIdFactory', () => {
   test(`takes a factory function and returns a component receiving a function
     child that gets an argument with a generated id`, () => {
     const factory = uniqueid('test')
     const Component = withIdFactory(factory)
-    const rendered = Enzyme.shallow(
+    const rendered = shallow(
       r(Component, { children: (id: string) => r('div', {}, id) }),
     )
     expect(rendered.at(0).childAt(0).text()).toBe('test0')
--- packages/react-sequential-id/src/index.ts
+++ packages/react-sequential-id/src/index.ts
@@ -1,5 +1,5 @@
 import { ReactElement, SFC, StatelessComponent } from 'react'
-import uniqueid from 'uniqueid'
+import uniqueid = require('uniqueid')
 
 export interface ISequentialIdProps {
   children: (id: string) => ReactElement<any> | null
