ec77b80284577234fe4a4e4a8c70a67676c22c22
Reidar Djupvik <mail@3rdhand.info>
2017-11-03 19:50:51 +0000

    Add typings and preliminary output

--- packages/react-input-feedback/src/index.ts
+++ packages/react-input-feedback/src/index.ts
@@ -1,3 +1,28 @@
-export default function Input() {
-  return null
+import { ComponentClass, createElement as r, SFC } from 'react'
+import { WrappedFieldProps } from 'redux-form'
+
+export type Component<P> = string | ComponentClass<P> | SFC<P>
+
+export interface IInputProps extends WrappedFieldProps {
+  components: {
+    error: Component<any>
+    input: Component<any>
+    wrapper: Component<any>
+  }
+}
+
+export default function Input({
+  components,
+  input,
+  meta,
+  ...props,
+}: IInputProps) {
+  const { error, touched } = meta
+  const showError = touched && !!error
+  return r(
+    components.wrapper,
+    {},
+    r(components.input, { ...props, ...input }),
+    showError && r(components.error),
+  )
 }
