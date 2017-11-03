28106e6d274cd00e9694a4aab48d8f7e58cebba9
Reidar Djupvik <mail@3rdhand.info>
2017-11-03 22:42:27 +0000

    Allow components to be omitted

--- packages/react-input-feedback/src/index.ts
+++ packages/react-input-feedback/src/index.ts
@@ -1,9 +1,11 @@
+import { defaultTo, lensProp, merge, over, pipe } from 'ramda'
 import { ComponentClass, createElement as r, SFC } from 'react'
+import { mapProps } from 'recompose'
 import { WrappedFieldProps } from 'redux-form'
 
 export type Component<P> = string | ComponentClass<P> | SFC<P>
 
-export interface IInputProps extends WrappedFieldProps {
+export interface IInputProps {
   components: {
     error: Component<any>
     input: Component<any>
@@ -11,12 +13,10 @@ export interface IInputProps extends WrappedFieldProps {
   }
 }
 
-export default function Input({
-  components,
-  input,
-  meta,
-  ...props,
-}: IInputProps) {
+export type InputProps = IInputProps & WrappedFieldProps
+export type PartialInputProps = Partial<IInputProps> & WrappedFieldProps
+
+function Input({ components, input, meta, ...props }: InputProps) {
   const { error, touched } = meta
   const showError = touched && !!error
   return r(
@@ -26,3 +26,17 @@ export default function Input({
     showError && r(components.error),
   )
 }
+
+const withDefaultComponents: (
+  component: Component<InputProps>,
+) => Component<PartialInputProps> = mapProps(
+  over(
+    lensProp('components'),
+    pipe(
+      defaultTo({}),
+      merge({ error: 'span', input: 'input', wrapper: 'span' }),
+    ),
+  ),
+)
+
+export default withDefaultComponents(Input)
