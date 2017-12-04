b6e407ccfb8d94c92b35b6154de8732969fb8e5f
Reidar Djupvik <mail@3rdhand.info>
2017-12-04 19:09:20 +0000

    Load JS stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react'
 
-const req = require.context('../packages', true, /\.stories\.ts$/)
+const req = require.context('../packages/', true, /\.stories\.js$/)
 
 function loadStories() {
   req.keys().forEach(req)
