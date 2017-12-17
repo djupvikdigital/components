3e4102bc234809f1009d876479e2ce7d79b6db8a
Reidar Djupvik <mail@3rdhand.info>
2017-12-17 23:36:06 +0000

    Add redux-form and react-redux to top level devDependencies

--- packages/react-input-feedback/src/index.stories.js
+++ packages/react-input-feedback/src/index.stories.js
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import { createElement as r } from 'react'
 
-import Input from './test.ts'
+import Input from './index.ts'
 
 storiesOf('InputFeedback', module).add('default', () => r(Input))
