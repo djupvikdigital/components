b192aabb4201be32c0c694d29a910fbebec33ae2
Reidar Djupvik <mail@3rdhand.info>
2017-12-26 15:49:03 +0000

    Create change handler

--- packages/react-submit/src/index.test.ts
+++ packages/react-submit/src/index.test.ts
@@ -1,8 +1,37 @@
-import { configure, render } from 'enzyme'
+import { configure, render, shallow } from 'enzyme'
 import Adapter = require('enzyme-adapter-react-16')
+import { mergeDeepRight } from 'ramda'
 import { createElement as r } from 'react'
 
 import SubmitButton from './'
+
+const createInputProps = mergeDeepRight({
+  input: {
+    name: 'input',
+    onBlur: null,
+    onChange: null,
+    onDragStart: null,
+    onDrop: null,
+    onFocus: null,
+    value: '',
+  },
+  meta: {
+    asyncValidating: null,
+    autofilled: null,
+    dirty: null,
+    dispatch: null,
+    error: null,
+    form: null,
+    initial: null,
+    invalid: null,
+    pristine: null,
+    submitFailed: null,
+    submitting: null,
+    touched: null,
+    valid: null,
+    visited: null,
+  },
+})
 
 configure({ adapter: new Adapter() })
 
@@ -13,5 +42,14 @@ describe('SubmitButton', () => {
     expect(wrapper.is('button')).toBe(true)
     expect(wrapper.attr('type')).toBe('submit')
     expect(wrapper.text()).toBe(buttonText)
+  })
+
+  it('takes an onChange prop with a callback function', () => {
+    const onChange = jest.fn()
+    const wrapper = shallow(
+      r(SubmitButton, createInputProps({ input: { onChange } })),
+    )
+    wrapper.simulate('click')
+    expect(onChange.mock.calls.length).toBe(1)
   })
 })
--- packages/react-submit/src/index.ts
+++ packages/react-submit/src/index.ts
@@ -1,10 +1,17 @@
 import { createElement as r, DetailedReactHTMLElement, HTMLProps } from 'react'
+import { WrappedFieldProps } from 'redux-form'
 
 export type ButtonProps = HTMLProps<HTMLButtonElement>
+export type SubmitButtonProps = ButtonProps & WrappedFieldProps
+
+function changeHandler(callback: any) {
+  return typeof callback === 'function' ? callback : null
+}
 
 export default function SubmitButton(
-  props: ButtonProps,
+  { children, input = { onChange: null }, ...props }: SubmitButtonProps,
 ): DetailedReactHTMLElement<ButtonProps, HTMLButtonElement> {
-  const { children } = props
-  return r('button', { ...props, type: 'submit' }, children)
+  const { onChange } = input
+  const onClick = changeHandler(onChange)
+  return r('button', { ...props, onClick, type: 'submit' }, children)
 }
