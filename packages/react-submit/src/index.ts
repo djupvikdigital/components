e8157f182f75c37d0f179b804a5e6ef29fa44a26
Reidar Djupvik <mail@3rdhand.info>
2017-12-29 16:18:56 +0000

    Allow component override

--- packages/react-submit/src/index.ts
+++ packages/react-submit/src/index.ts
@@ -1,18 +1,25 @@
 import * as React from 'react'
 import { WrappedFieldProps } from 'redux-form'
 
+export interface ISubmitButtonProps {
+  component: string
+}
+
 export type ButtonProps = React.HTMLProps<HTMLButtonElement>
-export type SubmitButtonProps = ButtonProps & WrappedFieldProps
+export type SubmitButtonProps = ButtonProps &
+  ISubmitButtonProps &
+  WrappedFieldProps
 
 const r = React.createElement
 
 export default function SubmitButton({
   children,
+  component = 'button',
   input = { name: '', onChange: null },
   meta,
   ...props
 }: SubmitButtonProps) {
   const { name, onChange } = input
   const onClick = typeof onChange === 'function' ? () => onChange(true) : null
-  return r('button', { ...props, name, onClick, type: 'submit' }, children)
+  return r(component, { ...props, name, onClick, type: 'submit' }, children)
 }
