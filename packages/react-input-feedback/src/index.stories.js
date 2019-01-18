c938129521247fb4a6643b31f54d9189a2fcb3d0
Reidar Djupvik <mail@3rdhand.info>
2019-01-18 22:04:35 +0000

    Create stories with styles

--- packages/react-input-feedback/src/index.stories.js
+++ packages/react-input-feedback/src/index.stories.js
@@ -1,10 +1,48 @@
 import { storiesOf } from '@storybook/react'
 import { createElement as r } from 'react'
+import { Client } from 'styletron-engine-atomic'
+import { Provider, styled } from 'styletron-react'
 
 import Input from './index.ts'
+
+const styletron = new Client()
+
+const colors = {
+  brand: '#808080',
+  error: 'red',
+}
+
+const StyledInput = styled('input', ({ 'aria-invalid': invalid }) => ({
+  border: '1px solid #808080',
+  outline: `2px solid ${invalid ? colors.error : 'transparent'}`,
+  outlineOffset: '-2px',
+  ':focus': {
+    border: `1px solid ${colors.brand}`,
+    outline: `2px solid ${invalid ? colors.error : colors.brand}`,
+  },
+}))
 
 storiesOf('InputFeedback', module)
   .add('default', () => r(Input))
   .add('with error', () =>
     r(Input, { meta: { error: 'Error text', touched: true } }),
+  )
+  .add('styled', () =>
+    r(
+      Provider,
+      { value: styletron },
+      r(Input, { components: { input: StyledInput } }),
+    ),
+  )
+  .add('styled with error', () =>
+    r(
+      Provider,
+      { value: styletron },
+      r(Input, {
+        components: {
+          input: StyledInput,
+        },
+        meta: { error: 'Error text', touched: true },
+      }),
+    ),
   )
