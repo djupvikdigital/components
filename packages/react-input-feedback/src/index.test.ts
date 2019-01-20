fe012a3e4ded0ccf73bffb900a6f4aca683a8e4f
Reidar Djupvik <mail@3rdhand.info>
2019-01-20 21:32:48 +0000

    Support a render callback function

--- packages/react-input-feedback/src/index.test.ts
+++ packages/react-input-feedback/src/index.test.ts
@@ -1,12 +1,25 @@
 import { configure, render } from 'enzyme'
 import Adapter = require('enzyme-adapter-react-16')
 import { mergeDeepRight } from 'ramda'
-import { createElement as r } from 'react'
+import { createElement as r, Fragment } from 'react'
 
-import Input from './'
+import Input, { StateAndHelpers } from './'
 
 function noop() {
   return
+}
+
+function customRender({
+  error,
+  getErrorProps,
+  getInputProps,
+}: StateAndHelpers) {
+  return r(
+    Fragment,
+    {},
+    r('textarea', getInputProps()),
+    r('div', getErrorProps({ className: 'bar' }), error),
+  )
 }
 
 const createInputProps = mergeDeepRight({
@@ -105,5 +118,31 @@ describe('Input', () => {
         .eq(1)
         .attr('id'),
     )
+  })
+
+  test('takes a function child for custom rendering', () => {
+    const className = 'foo'
+    const wrapper = render(
+      r(
+        'div',
+        {},
+        r(
+          Input,
+          createInputProps({
+            className,
+            meta: { error: 'error', touched: true },
+          }),
+          customRender,
+        ),
+      ),
+    )
+    const input = wrapper.children().eq(0)
+    const errorElement = wrapper.children().eq(1)
+    const describedby = input.attr('aria-describedby')
+    expect(input.is('textarea')).toBe(true)
+    expect(input.attr('class')).toBe(className)
+    expect(errorElement.attr('class')).toBe('bar')
+    expect(describedby).toBeDefined()
+    expect(describedby).toBe(errorElement.attr('id'))
   })
 })
--- packages/react-input-feedback/src/index.ts
+++ packages/react-input-feedback/src/index.ts
@@ -2,15 +2,42 @@ import {
   Component,
   ComponentElement,
   createElement as r,
+  FC,
   Fragment,
   ReactElement,
+  ReactNode,
   ReactPortal,
   SFCElement,
 } from 'react'
 import { FieldRenderProps } from 'react-final-form'
 import SequentialId, { ISequentialIdProps } from 'react-sequential-id'
 
-export type InputProps = FieldRenderProps
+export interface ErrorProps {
+  id: string
+}
+
+export interface InputProps {
+  'aria-describedby': string | null
+  'aria-invalid': boolean
+}
+
+export interface PropGetters {
+  getErrorProps: (options?: object) => ErrorProps
+  getInputProps: (options?: object) => InputProps
+}
+
+export interface State {
+  error: any
+}
+
+export type StateAndHelpers = PropGetters & State
+
+export type RenderCallback = (stateAndHelpers: StateAndHelpers) => ReactNode
+
+export interface InputFeedbackProps extends FieldRenderProps {
+  children?: RenderCallback
+  render?: RenderCallback
+}
 
 export {
   Component,
@@ -21,24 +48,48 @@ export {
   SFCElement,
 }
 
-export default function InputFeedback({
+function getErrorProps(errorId: string) {
+  return (options = {}) => ({ ...options, id: errorId })
+}
+
+function getInputProps(showError: boolean, errorId: string, props: object) {
+  return (options = {}) => ({
+    ...props,
+    ...options,
+    'aria-describedby': showError ? errorId : null,
+    'aria-invalid': showError,
+  })
+}
+
+const InputFeedback: FC<InputFeedbackProps> = function InputFeedback({
+  children,
   input,
   meta = { error: null, touched: false },
+  render,
   ...props
-}: InputProps) {
+}) {
+  const inputProps = { ...props, ...input }
   const { error, touched } = meta
-  const showError = touched && !!error
-  return r(SequentialId, {}, (errorId: string) =>
-    r(
+  const showError = Boolean(touched && !!error)
+  return r(SequentialId, {}, (errorId: string) => {
+    const options = {
+      error,
+      getErrorProps: getErrorProps(errorId),
+      getInputProps: getInputProps(showError, errorId, inputProps),
+    }
+    if (typeof render === 'function') {
+      return render(options)
+    }
+    if (typeof children === 'function') {
+      return children(options)
+    }
+    return r(
       Fragment,
       {},
-      r('input', {
-        'aria-describedby': showError ? errorId : null,
-        'aria-invalid': showError,
-        ...props,
-        ...input,
-      }),
-      showError && r('span', { id: errorId }, error),
-    ),
-  )
+      r('input', options.getInputProps()),
+      showError && r('span', options.getErrorProps(), error),
+    )
+  })
 }
+
+export default InputFeedback
